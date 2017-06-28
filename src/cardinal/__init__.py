553c8f8f8c51753459fc5023cbeed0c5d1d69324
Simon Veit Engmann <simon.engmann@world-of-gaming-community.de>
2017-06-28 06:43:55 +0200

    Fixed name overlaps apparently breaking the script on Py3.5

--- src/cardinal/__init__.py
+++ src/cardinal/__init__.py
@@ -1,6 +1,6 @@
 import logging
 import discord
-import discord.ext.commands as commands
+import discord.ext.commands as _commands
 
 import cardinal.utils as utils
 from __main__ import config
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # TODO: Implement server-specific prefixes
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(config['cmd_prefix']), description='cardinal.py')
+bot = _commands.Bot(command_prefix=_commands.when_mentioned_or(config['cmd_prefix']), description='cardinal.py')
 
 
 @bot.event
@@ -23,27 +23,27 @@ async def on_ready():
 async def on_command_error(ex, ctx):
     error_msg = ''
 
-    if isinstance(ex, commands.errors.CheckFailure):
+    if isinstance(ex, _commands.errors.CheckFailure):
         error_msg = 'You are not allowed to use this command (here).'
-    elif isinstance(ex, commands.errors.MissingRequiredArgument):
+    elif isinstance(ex, _commands.errors.MissingRequiredArgument):
         error_msg = 'Too few arguments.'
-    elif isinstance(ex, commands.errors.TooManyArguments):
+    elif isinstance(ex, _commands.errors.TooManyArguments):
         error_msg = 'Too many arguments.'
-    elif isinstance(ex, commands.errors.BadArgument):
+    elif isinstance(ex, _commands.errors.BadArgument):
         error_msg = 'Arguments parsing failed.'
-    elif isinstance(ex, commands.errors.NoPrivateMessage):
+    elif isinstance(ex, _commands.errors.NoPrivateMessage):
         error_msg = 'Command must not be used in private message channels.'
-    elif isinstance(ex, commands.errors.CommandOnCooldown):
+    elif isinstance(ex, _commands.errors.CommandOnCooldown):
         error_msg = str(ex)
 
-    if isinstance(ex, commands.errors.UserInputError):
+    if isinstance(ex, _commands.errors.UserInputError):
         error_msg += ' See `{}help {}` for information on the command.'\
             .format(utils.clean_prefix(ctx), ctx.command.qualified_name)
 
     if error_msg != '':
         await bot.send_message(ctx.message.channel, error_msg)
 
-    if isinstance(ex, commands.errors.CommandInvokeError):
+    if isinstance(ex, _commands.errors.CommandInvokeError):
         logger.log(logging.ERROR, utils.format_exception(ex.original))
 
 
