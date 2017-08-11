ff1cf0a64ff2ce58737d6b8d6b9e562891375e49
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-08-11 04:16:20 +0200

    Lots of changes around newbie functionality, including convencience commands for changing configuration

--- src/cardinal/commands/newbie.py
+++ src/cardinal/commands/newbie.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 import logging
 import re
 import traceback
@@ -8,19 +9,49 @@ import discord.ext.commands as commands
 
 from cardinal.commands import Cog
 from cardinal.db import session_scope
-from cardinal.db.newbie import User, Guild
+from cardinal.db.newbie import User, Guild, Channel
 from cardinal.utils import clean_prefix, format_discord_user, format_discord_guild
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+def newbie_enabled(func):
+    """Decorator to check if newbie roling is enabled before running the command."""
+
+    if isinstance(func, commands.Command):
+        cmd = func.callback
+    else:
+        cmd = func
+
+    @functools.wraps(cmd)
+    async def wrapper(*args, **kwargs):
+        _self = args[0]
+        ctx = args[1]
+        if ctx:
+            with session_scope() as session:
+                if not (ctx.message.server and session.query(Guild).get(ctx.message.server.id)):
+                    await _self.bot.say('Newbie roling is not enabled on this server. Please enable it before using these commands.')
+                    return
+
+        await cmd(*args, **kwargs)
+
+    if isinstance(func, commands.Command):
+        func.callback = wrapper
+        return func
+    else:
+        return wrapper
+
+
 class Newbies(Cog):
     channel_re = re.compile(
         r'((<#)|^)(?P<id>\d+)(?(2)>|(\s|$))')  # Matches either a channel mention of the form "<#id>" or a raw ID. The actual ID can be extracted from the 'id' group of the match object
 
+    everyone_overwrite = discord.PermissionOverwrite(read_messages=True)
+
     def __init__(self, bot):
         super().__init__(bot)
+        # TODO: Scan guilds for non-members who are not in the DB i.e. who joined during a bot downtime
 
     async def on_member_join(self, member: discord.Member):
         with session_scope() as session:
@@ -29,25 +60,22 @@ class Newbies(Cog):
             if guild is None:
                 return
 
-            paginator = commands.formatter.Paginator(prefix='', suffix='')
-            for line in guild.welcome_message.splitlines():
-                paginator.add_line(line)
+            message_content = guild.welcome_message
 
-            paginator.add_line()
-            paginator.add_line(
-                'Please reply with the following message to be granted access to "{}".'.format(member.server.name))
-            paginator.add_line('```{}```'.format(guild.response_message))
+            message_content += '\n'
+            message_content += 'Please reply with the following message to be granted access to "{}".\n'.format(member.server.name)
+            message_content += '```{}```'.format(guild.response_message)
 
-            user = User(guildid=member.server.id, userid=member.id, joined_at=member.joined_at)
+            message = await self.bot.send_message(member, message_content)
+
+            user = User(guildid=member.server.id, userid=member.id, messageid=message.id, joined_at=member.joined_at)
             session.add(user)
-
-            for page in paginator.pages:
-                await self.bot.send_message(member, page)
 
             await self.bot.send_message(member, 'Please note that by staying on "{}", you agree that this bot stores your user ID for identification purposes.\nIt shall be deleted once you confirm the above message or leave the server.'.format(member.server.name))  # Necessary in compliance with Discord's latest ToS changes ¯\_(ツ)_/¯
 
-    async def on_member_leave(self, member: discord.Member):
+    async def on_member_remove(self, member: discord.Member):
         with session_scope() as session:
+            # Using query instead of object deletion to prevent redundant SELECT query
             session.query(User).filter(User.userid == member.id, User.guildid == member.server.id).delete(synchronize_session=False)  # Necessary in compliance with Discord's latest ToS changes ¯\_(ツ)_/¯
 
     async def on_message(self, msg: discord.Message):
@@ -66,17 +94,17 @@ class Newbies(Cog):
 
                 guild = self.bot.get_server(db_user.guildid)
                 if not guild:
-                    session.delete(db_user)
-                    session.delete(db_guild)
+                    # session.delete(db_user)  # Unnecessary because ON DELETE CASCADE / 'delete-orphan' should clean it up automatically
+                    session.delete(db_guild)  # Delete guild if bot no longer has access to it
                     continue
 
                 member = guild.get_member(msg.author.id)
                 if not member:
-                    session.delete(db_user)
+                    session.delete(db_user)  # Delete row if user already left
                     continue
 
                 if db_guild.timeout:
-                    joined_interval = datetime.datetime.utcnow() - db_user.joined_at
+                    joined_interval = datetime.datetime.utcnow() - db_user.joined_at  # Use utcnow because discord.py's join timestamps are in UTC
                     if joined_interval >= db_guild.timeout:
                         try:
                             await self.bot.kick(member)
@@ -111,17 +139,18 @@ class Newbies(Cog):
                     logger.log(logging.ERROR, 'Failed managing roles for user {} on guild {} due to HTTP error {}.'
                                .format(format_discord_user(member), format_discord_guild(guild), e.response.status))
                     logger.log(logging.ERROR, traceback.format_exc())
-                else:
-                    session.delete(db_user)
+
+                session.delete(db_user)
 
     @commands.group(pass_context=True, no_pm=True)
     @commands.has_permissions(manage_server=True)
     @commands.bot_has_permissions(manage_roles=True, manage_channels=True)
     async def newbie(self, ctx: commands.Context):
         """Provides functionality for managing automatic newbie roling."""
+
         if ctx.invoked_subcommand is None:
-            await self.bot.say('Invalid subcommand passed, please refer to {}help {} for further information.'.format(
-                clean_prefix(ctx), ctx.command.qualified_name))
+            await self.bot.say('Invalid subcommand passed, please refer to `{}help {}` for further information.'
+                               .format(clean_prefix(ctx), ctx.command.qualified_name))
 
     @newbie.command(pass_context=True)
     async def enable(self, ctx: commands.Context):
@@ -138,36 +167,31 @@ class Newbies(Cog):
             everyone_permissions.send_messages = False
             everyone_permissions.read_message_history = False
             member_permissions = discord.Permissions(0x400 | 0x800 | 0x10000)
-            overwrite = discord.PermissionOverwrite(read_messages=True)
 
-            await self.bot.say(
-                'Please enter the channels that are to remain visible to newbies, separated by spaces.\n_Takes channel mentions, names, and IDs._')
-            channels_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author,
-                                                               channel=ctx.message.channel)
+            await self.bot.say('Please enter the channels that are to remain visible to newbies, separated by spaces.\n_Takes channel mentions, names, and IDs._')
+            channels_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author, channel=ctx.message.channel)
             if not channels_message:
                 await self.bot.say('Terminating process due to timeout.')
                 return
 
             await self.bot.say('Enter the welcome message that should be displayed to new users.')
-            welcome_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author,
-                                                              channel=ctx.message.channel)
+            welcome_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author, channel=ctx.message.channel)
             if not welcome_message:
                 await self.bot.say('Terminating process due to timeout.')
                 return
 
             await self.bot.say('Enter the message the user has to respond with.')
-            response_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author,
-                                                               channel=ctx.message.channel)
+            response_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author, channel=ctx.message.channel)
             if not response_message:
                 await self.bot.say('Terminating process due to timeout.')
                 return
 
             await self.bot.say('Enter a timeout for new users in hours. Enter 0 to disable timeouts.')
-            timeout_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author,
-                                                              channel=ctx.message.channel)
+            timeout_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author, channel=ctx.message.channel)
             if not timeout_message:
                 await self.bot.say('Terminating process due to timeout.')
                 return
+
             try:
                 timeout_int = int(timeout_message.content.strip())
             except ValueError:
@@ -180,19 +204,6 @@ class Newbies(Cog):
 
             await self.bot.edit_role(ctx.message.server, everyone_role, permissions=everyone_permissions)
 
-            for channel in channels_message.content.split():
-                match = self.channel_re.match(channel)
-                if match:
-                    channel_id = match.group('id')
-                    channel_obj = discord.utils.get(ctx.message.server.channels, id=channel_id)
-                    if channel_obj:
-                        await self.bot.edit_channel_permissions(channel_obj, everyone_role, overwrite)
-
-                else:
-                    channel_obj = discord.utils.get(ctx.message.server.channels, name=channel.lower())
-                    if channel_obj:
-                        await self.bot.edit_channel_permissions(channel_obj, member_role, overwrite)
-
             db_guild = Guild(guildid=ctx.message.server.id, roleid=member_role.id,
                              welcome_message=welcome_message.content.strip(),
                              response_message=response_message.content.strip())
@@ -202,11 +213,27 @@ class Newbies(Cog):
 
             session.add(db_guild)
 
+            for channel in channels_message.content.split():
+                match = self.channel_re.match(channel)
+                if match:
+                    channel_id = match.group('id')
+                    channel_obj = discord.utils.get(ctx.message.server.channels, id=channel_id)
+                    if channel_obj and channel_obj.server.id == ctx.message.server.id:
+                        await self.bot.edit_channel_permissions(channel_obj, everyone_role, self.everyone_overwrite)
+                        db_channel = Channel(channelid=channel_obj.id, guildid=ctx.message.server.id)
+                        session.add(db_channel)
+
+                else:
+                    channel_obj = discord.utils.get(ctx.message.server.channels, name=channel.lower())
+                    if channel_obj:
+                        await self.bot.edit_channel_permissions(channel_obj, member_role, self.everyone_overwrite)
+
         await self.bot.say('Automatic newbie roling is now enabled for this server.')
 
     @newbie.command(pass_context=True)
     async def disable(self, ctx: commands.Context):
         """Disables automatic newbie roling for this server."""
+
         with session_scope() as session:
             db_guild = session.query(Guild).get(ctx.message.server.id)
             if not db_guild:
@@ -217,10 +244,11 @@ class Newbies(Cog):
                 await self.bot.say('Role has already been deleted.')
 
             everyone_role = ctx.message.server.default_role
-            everyone_permissions = everyone_role.permissions
-            everyone_permissions.read_messages = True
-            everyone_permissions.send_messages = True
-            everyone_permissions.read_message_history = True
+            # everyone_permissions = everyone_role.permissions
+            # everyone_permissions.read_messages = True
+            # everyone_permissions.send_messages = True
+            # everyone_permissions.read_message_history = True
+            everyone_permissions = role.permissions
 
             await self.bot.edit_role(ctx.message.server, everyone_role, permissions=everyone_permissions)
 
@@ -228,3 +256,129 @@ class Newbies(Cog):
             session.delete(db_guild)
 
         await self.bot.say('Disabled newbie roling for this server.')
+
+    @newbie.command(pass_context=True)
+    @newbie_enabled
+    async def timeout(self, ctx: commands.Context, delay: float = 0.0):
+        """
+        Sets the timeout in hours before new users get kicked.
+        Put a non-positive value (zero or less) or nothing to remove it.
+        """
+
+        with session_scope() as session:
+            db_guild = session.query(Guild).get(ctx.message.server.id)
+
+            if delay > 0:
+                db_guild.timeout = datetime.timedelta(hours=delay)
+            else:
+                db_guild.timeout = None
+
+        await self.bot.say('Successfully set timeout to {} hours.'.format(delay))
+
+    @newbie.command(pass_context=True, name='welcome-message')
+    @newbie_enabled
+    async def _welcome_message(self, ctx: commands.Context):
+        """Sets the welcome message for the server."""
+
+        await self.bot.say('Please enter the message you would like to display to new users.')
+        welcome_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author, channel=ctx.message.channel)
+        if not welcome_message:
+            await self.bot.say('Terminating process due to timeout.')
+            return
+
+        with session_scope() as session:
+            db_guild = session.query(Guild).get(ctx.message.server.id)
+            db_guild.welcome_message = welcome_message.content
+
+        await self.bot.say('Successfully set welcome message.')
+
+    @newbie.command(pass_context=True, name='response-message')
+    @newbie_enabled
+    async def _response_message(self, ctx: commands.Context, *, msg: str = None):
+        """Sets the response message for the server."""
+
+        if not msg:
+            await self.bot.say('Please enter the response message users have to enter upon joining the server.')
+            response_message = await self.bot.wait_for_message(timeout=60.0, author=ctx.message.author, channel=ctx.message.channel)
+            if not response_message:
+                await self.bot.say('Terminating process due to timeout.')
+                return
+
+            msg = response_message.content
+
+        with session_scope() as session:
+            db_guild = session.query(Guild).get(ctx.message.server.id)
+            db_guild.response_message = msg
+
+        await self.bot.say('Successfully set response message.')
+
+    @newbie.group(pass_context=True)
+    async def channels(self, ctx: commands.Context):
+        """Modifies the channels unconfirmed users can access."""
+
+        if ctx.invoked_subcommand is None:
+            await self.bot.say('Invalid subcommand passed, please refer to `{}help {}` for further information.\nValid options include `add`, `remove` and `list`.'
+                               .format(clean_prefix(ctx), ctx.command.qualified_name))
+            return
+
+    @channels.command(pass_context=True)
+    @newbie_enabled
+    async def add(self, ctx: commands.Context, *, channel: discord.Channel):
+        """Adds a channel to the list of channels unconfirmed users can access."""
+
+        if not channel.server.id == ctx.message.server.id:
+            await self.bot.say('The provided channel is not on this server.')
+            return
+
+        with session_scope() as session:
+            if session.query(Channel).get(channel.id):
+                await self.bot.say('This channel is already visible to unconfirmed users.')
+                return
+
+            everyone_role = ctx.message.server.default_role
+            await self.bot.edit_channel_permissions(channel, everyone_role, self.everyone_overwrite)
+
+            db_channel = Channel(channelid=channel.id, guildid=ctx.message.server.id)
+            session.add(db_channel)
+
+        await self.bot.say('{} is now visible to unconfirmed users.'.format(channel.mention))
+
+    @channels.command(pass_context=True)
+    @newbie_enabled
+    async def remove(self, ctx: commands.Context, *, channel: discord.Channel):
+        """Removes a channel from the list of channels unconfirmed users can access."""
+
+        if not channel.server.id == ctx.message.server.id:
+            await self.bot.say('The provided channel is not on this server.')
+            return
+
+        with session_scope() as session:
+            db_channel = session.query(Channel).get(channel.id)
+            if not db_channel:
+                await self.bot.say('The provided channel is not visible to unconfirmed members.')
+                return
+
+            everyone_role = ctx.message.server.default_role
+            await self.bot.edit_channel_permissions(channel, everyone_role)
+
+            session.delete(db_channel)
+
+        await self.bot.say('{} is now invisble to unconfirmed users.'.format(channel.mention))
+
+    @channels.command(pass_context=True)
+    @newbie_enabled
+    async def list(self, ctx: commands.Context):
+        """Lists the channels visible to unconfirmed users of this server."""
+
+        with session_scope() as session:
+            answer = 'The following channels are visible to unconfirmed users of this server.```'
+            for db_channel in session.query(Channel).filter(Channel.guildid == ctx.message.server.id):
+                channel = discord.utils.get(ctx.message.server.channels, id=db_channel.channelid)
+                if channel:
+                    answer += '#'
+                    answer += channel.name
+                    answer += '\n'
+
+            answer += '```'
+
+        await self.bot.say(answer)
--- src/cardinal/db/newbie.py
+++ src/cardinal/db/newbie.py
@@ -17,10 +17,18 @@ class Guild(db.Base):
 class User(db.Base):
     __tablename__ = 'newbie_users'
 
-    guildid = Column(String, ForeignKey(Guild.guildid), primary_key=True, autoincrement=False)
     userid = Column(String, primary_key=True, autoincrement=False)
+    guildid = Column(String, ForeignKey(Guild.guildid), primary_key=True, autoincrement=False)
+    messageid = Column(String, unique=True, nullable=False)
     joined_at = Column(DateTime, nullable=False)
 
 
+class Channel(db.Base):
+    __tablename__ = 'newbie_channels'
+    channelid = Column(String, primary_key=True, autoincrement=False)
+    guildid = Column(String, ForeignKey(Guild.guildid), nullable=False)
+
+
 Guild.users = relationship(User, backref='guild', innerjoin=True, cascade='all, delete-orphan', lazy=True)  # TODO: Decide on lazy (True) or eager (False) loading
+Guild.channels = relationship(Channel, backref='guild', cascade='all, delete-orphan', lazy=True)
 db.Base.metadata.create_all(db.engine)
