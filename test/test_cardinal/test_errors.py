0027a39a077bd4a6d827671fb6243a9ee47734b7
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-09-30 12:32:54 +0200

    Added tests for errors.py

---
+++ test/test_cardinal/test_errors.py
@@ -0,0 +1,30 @@
+import unittest as ut
+
+from . import Empty
+import cardinal.errors as errors
+
+
+class ChannelNotWhitelistedTestCase(ut.TestCase):
+    def setUp(self):
+        ctx = Empty()
+        ctx.channel = Empty()
+        ctx.channel.mention = '<#123456789>'
+        self.ctx = ctx
+
+    def test_ChannelNotWhitelisted_ctor(self):
+        ex = errors.ChannelNotWhitelisted(self.ctx)
+        expected = 'Channel {} is not whitelisted.'.format(self.ctx.channel.mention)
+        got = str(ex)
+        self.assertMultiLineEqual(expected, got)
+        self.assertIs(self.ctx.channel, ex.channel)
+
+
+class UserBlacklistedTestCase(ut.TestCase):
+    def setUp(self):
+        ctx = Empty()
+        ctx.author = Empty()
+        self.ctx = ctx
+
+    def test_UserBlacklisted_ctor(self):
+        ex = errors.UserBlacklisted(self.ctx)
+        self.assertIs(self.ctx.author, ex.user)
