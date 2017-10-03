16af804cea259f13b8ad602e5f822e7648d311ba
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-10-03 18:25:40 +0200

    Further changes to mock usage
    Added arguments to CoroMock to pass through to underlying coro
    Replaced engine with mock since not explicitly used
    Switched to PropertyMock for `Bot.user`

--- test/test_cardinal/__init__.py
+++ test/test_cardinal/__init__.py
@@ -2,8 +2,8 @@ from asyncio import coroutine
 from unittest.mock import Mock
 
 
-def CoroMock():
-    coro = Mock(name="CoroutineResult")
+def CoroMock(*args, **kwargs):
+    coro = Mock(name="CoroutineResult", *args, **kwargs)
     corofunc = Mock(name="CoroutineFunction", side_effect=coroutine(coro))
     corofunc.coro = coro
     return corofunc
--- test/test_cardinal/test___init__.py
+++ test/test_cardinal/test___init__.py
@@ -4,13 +4,12 @@ from asyncio import get_event_loop
 
 import discord
 import discord.ext.commands as commands
-from sqlalchemy import create_engine
 
 from . import CoroMock
 from cardinal import Bot
 
 default_game = 'Test game'
-engine = create_engine('sqlite:///')
+engine = mock.NonCallableMock()
 bot = Bot(default_game=default_game, engine=engine, command_prefix='Test prefix')
 loop = get_event_loop()
 
@@ -26,15 +25,15 @@ class BotCtorTestCase(ut.TestCase):
         self.assertIs(bot.engine, engine)
 
 
-@mock.patch.object(commands.Bot, 'user', 'Test user#1234')
+@mock.patch.object(commands.Bot, 'user', new_callable=mock.PropertyMock, return_value='Test user#1234')
 @mock.patch.object(commands.Bot, 'change_presence', new_callable=CoroMock)
 @mock.patch('cardinal.create_all')
 class BotOnReadyTestCase(ut.TestCase):
-    def test(self, create_all, change_presence):
+    def test(self, create_all, change_presence, user):
         with self.assertLogs('cardinal') as log:
             loop.run_until_complete(bot.on_ready())
 
-        self.assertMultiLineEqual(log.output[0], 'INFO:cardinal:Logged into Discord as {}'.format(bot.user))
+        self.assertMultiLineEqual(log.output[0], 'INFO:cardinal:Logged into Discord as {}'.format(user.return_value))
         create_all.assert_called_once_with(bot.engine)
         change_presence.assert_called_once_with(game=discord.Game(name=bot.default_game))
 
