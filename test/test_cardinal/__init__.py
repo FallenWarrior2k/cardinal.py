c112c5b41cf3520c7a6daad8cc1c9d6f6ea0051c
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-10-03 02:40:29 +0200

    Added function to use coroutines with mocks

--- test/test_cardinal/__init__.py
+++ test/test_cardinal/__init__.py
@@ -1,3 +1,14 @@
+from asyncio import coroutine
+from unittest.mock import Mock
+
+
 class Empty:
     """Centralised class to allow for dynamic properties."""
     pass
+
+
+def CoroMock():
+    coro = Mock(name="CoroutineResult")
+    corofunc = Mock(name="CoroutineFunction", side_effect=coroutine(coro))
+    corofunc.coro = coro
+    return corofunc
