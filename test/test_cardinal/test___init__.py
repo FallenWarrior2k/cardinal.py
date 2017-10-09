a6bf0e5c2175bca7c99ee6ec3cb1f6fbd63e5928
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-10-09 15:13:31 +0200

    Continued adding tests for Bot class

--- test/test_cardinal/test___init__.py
+++ test/test_cardinal/test___init__.py
@@ -1,12 +1,14 @@
+import logging
 import unittest as ut
 import unittest.mock as mock
 from asyncio import get_event_loop
 
 import discord
 import discord.ext.commands as commands
+from sqlalchemy.orm import Session
 
 from . import CoroMock
-from cardinal import Bot
+from cardinal import Bot, errors
 
 default_game = 'Test game'
 engine = mock.NonCallableMock()
@@ -23,6 +25,30 @@ class BotCtorTestCase(ut.TestCase):
     def test(self):
         self.assertIs(bot.default_game, default_game)
         self.assertIs(bot.engine, engine)
+
+
+class BotSessionScopeTestCase(ut.TestCase):
+    @mock.patch.object(bot, 'sessionmaker', return_value=mock.NonCallableMock(spec=Session))
+    def test_no_exception(self, sessionmaker):
+        with bot.session_scope() as session:
+            self.assertIsInstance(session, Session)
+
+        sessionmaker.assert_called_once_with()
+        sessionmaker.return_value.commit.assert_called_once_with()
+        sessionmaker.return_value.close.assert_called_once_with()
+
+    @mock.patch.object(bot, 'sessionmaker', return_value=mock.NonCallableMock(spec=Session))
+    def test_exception(self, sessionmaker):
+        exc_message = 'Test exception message'
+        exc = Exception(exc_message)
+        with self.assertRaises(Exception, msg=exc_message):
+            with bot.session_scope() as session:
+                self.assertIsInstance(session, Session)
+                raise exc
+
+        sessionmaker.assert_called_once_with()
+        sessionmaker.return_value.rollback.assert_called_once_with()
+        sessionmaker.return_value.close.assert_called_once_with()
 
 
 @mock.patch.object(commands.Bot, 'user', new_callable=mock.PropertyMock, return_value='Test user#1234')
@@ -68,6 +94,65 @@ class BotOnCommandTestCase(ut.TestCase):
         format_message.assert_called_once_with(ctx.message)
 
 
-@mock.patch('cardinal.utils.clean_prefix')
+class BotOnCommandCompletionTestCase(ut.TestCase):
+    def test(self):
+        ctx = mock.NonCallableMock()
+        loop.run_until_complete(bot.on_command_completion(ctx))
+
+
+@mock.patch('cardinal.utils.clean_prefix', return_value='Test prefix')
+@mock.patch('cardinal.logger', spec=logging.Logger)
 class BotOnCommandErrorTestCase(ut.TestCase):
-    pass
+    def setUp(self):
+        ctx = mock.NonCallableMock()
+        ctx.command.qualified_name = 'Test command name'
+        ctx.message.content = 'Test message content'
+
+        output = []
+        ctx.send = CoroMock(side_effect=output.append)
+        self.ctx = ctx
+        self.output = output
+
+    def test_command_error(self, logger, clean_prefix):
+        error = mock.NonCallableMock(spec=commands.CommandError)
+        loop.run_until_complete(bot.on_command_error(self.ctx, error))
+
+        logger.error.assert_not_called()
+        clean_prefix.assert_not_called()
+        self.ctx.send.assert_not_called()
+
+    def test_missing_required_argument(self, logger, clean_prefix):
+        error = mock.NonCallableMock(spec=commands.MissingRequiredArgument)
+        loop.run_until_complete(bot.on_command_error(self.ctx, error))
+
+        logger.error.assert_not_called()
+        clean_prefix.assert_called_once_with(self.ctx)
+        error_msg = 'Too few arguments. Did you forget anything?\nSee `{}help {}` for information on the command.'\
+            .format(clean_prefix.return_value, self.ctx.command.qualified_name)
+        self.ctx.send.assert_called_once_with(error_msg)
+        self.assertIn(error_msg, self.output)
+
+    def test_bad_argument(self, logger, clean_prefix):
+        error = mock.NonCallableMock(spec=commands.BadArgument)
+        loop.run_until_complete(bot.on_command_error(self.ctx, error))
+
+        logger.error.assert_not_called()
+        clean_prefix.assert_called_once_with(self.ctx)
+        error_msg = 'Argument parsing failed. Did you mistype anything?\nSee `{}help {}` for information on the command.'\
+            .format(clean_prefix.return_value, self.ctx.command.qualified_name)
+        self.ctx.send.assert_called_once_with(error_msg)
+        self.assertIn(error_msg, self.output)
+
+    def test_no_private_message(self, logger, clean_prefix):
+        error = mock.NonCallableMock(spec=commands.NoPrivateMessage)
+        loop.run_until_complete(bot.on_command_error(self.ctx, error))
+
+        logger.error.assert_not_called()
+        clean_prefix.assert_not_called()
+        error_msg = 'Command cannot be used in private message channels.'\
+            .format(clean_prefix.return_value, self.ctx.command.qualified_name)
+        self.ctx.send.assert_called_once_with(error_msg)
+        self.assertIn(error_msg, self.output)
+
+    def test_check_failure(self, logger, clean_prefix):
+        self.fail()
