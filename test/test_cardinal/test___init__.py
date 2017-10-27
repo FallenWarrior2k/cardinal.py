b6da32320377bbb2d645d399ce7a5dfedb56b6e9
Simon Engmann <simon.engmann@tu-dortmund.de>
2017-10-28 00:19:28 +0200

    Removed unnecessary patch

--- test/test_cardinal/test___init__.py
+++ test/test_cardinal/test___init__.py
@@ -3,7 +3,6 @@ import unittest as ut
 import unittest.mock as mock
 from asyncio import get_event_loop
 
-import discord
 import discord.ext.commands as commands
 from sqlalchemy.orm import Session, sessionmaker
 
@@ -104,7 +103,6 @@ class BotOnCommandCompletionTestCase(ut.TestCase):
 
 
 @mock.patch('cardinal.utils.clean_prefix', return_value='Test prefix')
-@mock.patch('cardinal.logger', autospec=True)
 class BotOnCommandErrorTestCase(ut.TestCase):
     def setUp(self):
         ctx = mock.NonCallableMock()
@@ -116,77 +114,69 @@ class BotOnCommandErrorTestCase(ut.TestCase):
         self.ctx = ctx
         self.output = output
 
-    def test_command_error(self, logger, clean_prefix):
+    def test_command_error(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.CommandError)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         self.ctx.send.assert_not_called()
 
-    def test_missing_required_argument(self, logger, clean_prefix):
+    def test_missing_required_argument(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.MissingRequiredArgument)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_called_once_with(self.ctx)
         error_msg = 'Too few arguments. Did you forget anything?\nSee `{}help {}` for information on the command.'\
             .format(clean_prefix.return_value, self.ctx.command.qualified_name)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_bad_argument(self, logger, clean_prefix):
+    def test_bad_argument(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.BadArgument)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_called_once_with(self.ctx)
         error_msg = 'Argument parsing failed. Did you mistype anything?\nSee `{}help {}` for information on the command.'\
             .format(clean_prefix.return_value, self.ctx.command.qualified_name)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_no_private_message(self, logger, clean_prefix):
+    def test_no_private_message(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.NoPrivateMessage)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         error_msg = 'Command cannot be used in private message channels.'\
             .format(clean_prefix.return_value, self.ctx.command.qualified_name)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_check_failure(self, logger, clean_prefix):
+    def test_check_failure(self, clean_prefix):
         error = mock.NonCallableMagicMock(spec=commands.CheckFailure)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         error_msg = 'This command cannot be used in this context.\n{}'.format(error.__str__.return_value)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_command_not_found(self, logger, clean_prefix):
+    def test_command_not_found(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.CommandNotFound)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         self.ctx.send.assert_not_called()
 
-    def test_disabled_command(self, logger, clean_prefix):
+    def test_disabled_command(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.DisabledCommand)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         self.ctx.send.assert_not_called()
 
-    def test_command_invoke_error(self, logger, clean_prefix):
+    def test_command_invoke_error(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.CommandInvokeError)
         error.original = mock.NonCallableMagicMock()
-        logger.error = logging.getLogger('cardinal').error
 
         with self.assertLogs('cardinal', logging.ERROR):
             loop.run_until_complete(bot.on_command_error(self.ctx, error))
@@ -196,72 +186,65 @@ class BotOnCommandErrorTestCase(ut.TestCase):
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_too_many_arguments(self, logger, clean_prefix):
+    def test_too_many_arguments(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.TooManyArguments)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_called_once_with(self.ctx)
         error_msg = 'Too many arguments. Did you miss any quotes?\nSee `{}help {}` for information on the command.'\
             .format(clean_prefix.return_value, self.ctx.command.qualified_name)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_user_input_error(self, logger, clean_prefix):
+    def test_user_input_error(self, clean_prefix):
         error = mock.NonCallableMock(spec=commands.UserInputError)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_called_once_with(self.ctx)
         error_msg = '\nSee `{}help {}` for information on the command.' \
             .format(clean_prefix.return_value, self.ctx.command.qualified_name)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_command_on_cooldown(self, logger, clean_prefix):
+    def test_command_on_cooldown(self, clean_prefix):
         error = mock.NonCallableMagicMock(spec=commands.CommandOnCooldown)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         error_msg = error.__str__.return_value
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_not_owner(self, logger, clean_prefix):
+    def test_not_owner(self, clean_prefix):
         error = mock.NonCallableMagicMock(spec=commands.NotOwner)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         error_msg = 'This command cannot be used in this context.\n{}'.format(error.__str__.return_value)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_missing_permissions(self, logger, clean_prefix):
+    def test_missing_permissions(self, clean_prefix):
         error = mock.NonCallableMagicMock(spec=commands.MissingPermissions)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         error_msg = 'This command cannot be used in this context.\n{}'.format(error.__str__.return_value)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_bot_missing_permissions(self, logger, clean_prefix):
+    def test_bot_missing_permissions(self, clean_prefix):
         error = mock.NonCallableMagicMock(spec=commands.BotMissingPermissions)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         error_msg = 'This command cannot be used in this context.\n{}'.format(error.__str__.return_value)
         self.ctx.send.assert_called_once_with(error_msg)
         self.assertIn(error_msg, self.output)
 
-    def test_user_blacklisted(self, logger, clean_prefix):
+    def test_user_blacklisted(self, clean_prefix):
         error = mock.NonCallableMock(spec=errors.UserBlacklisted)
         loop.run_until_complete(bot.on_command_error(self.ctx, error))
 
-        logger.error.assert_not_called()
         clean_prefix.assert_not_called()
         self.ctx.send.assert_not_called()
