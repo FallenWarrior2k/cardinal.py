2f7770a6718a7d8a4be4777bd2947f3fba8e7d10
Simon Engmann <simon.engmann@tu-dortmund.de>
2018-03-11 01:05:28 +0100

    Pytest accomodations
    Removed a file that became irrelevant after switching to pytest
    Renamed a test file that caused ambiguities

--- tests/test_cardinal/test_db/__init__.py
+++
@@ -1,8 +0,0 @@
-import os.path as path
-
-
-def load_tests(loader, standard_tests, pattern):
-    _dir = path.dirname(__file__)
-    package_tests = loader.discover(start_dir=_dir, pattern=pattern)
-    standard_tests.addTests(package_tests)
-    return standard_tests
