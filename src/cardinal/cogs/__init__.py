4a99c91bbe49874041f72b074e7a40b74aa2a5ef
Simon Engmann <simon.engmann@tu-dortmund.de>
2018-11-25 14:06:42 +0100

    Unfuck cog loading mechanism

--- src/cardinal/cogs/__init__.py
+++ src/cardinal/cogs/__init__.py
@@ -1,31 +1,18 @@
-# Please kindly ignore this craftiness
-
-import importlib
 import logging
-import pkgutil
 
 from .basecog import BaseCog
+from .channels import Channels
+from .moderation import Moderation
+from .newbie import Newbies
+from .roles import Roles
+from .whitelist import Whitelisting
 
 logger = logging.getLogger(__name__)
-
-
-def all_subclasses(cls):
-    yield from cls.__subclasses__()
-    yield from (g for s in cls.__subclasses__() for g in all_subclasses(s))
+cogs = (Channels, Moderation, Newbies, Roles, Whitelisting)
 
 
 def setup(bot):
-    imports = []
-    for _, mod_name, is_pkg in pkgutil.iter_modules(__path__):
-        if is_pkg:
-            continue
-
-        try:  # Ensure one borked module does not kill the whole bot
-            imports.append(importlib.import_module('.' + mod_name, __name__))
-        except Exception:
-            logger.exception('Error while importing "{}.{}".'.format(__name__, mod_name))
-
-    for cog in all_subclasses(BaseCog):
+    for cog in cogs:
         logger.info('Initializing "{}".'.format(cog.__name__))
         try:
             bot.add_cog(cog(bot))
--- tests/test_cardinal/test_cogs/test_cogs__init__.py
+++ tests/test_cardinal/test_cogs/test_cogs__init__.py
@@ -1,4 +1,3 @@
-import logging
 import unittest.mock as mock
 
 import pytest
@@ -7,167 +6,41 @@ from discord.ext import commands
 from cardinal import cogs
 
 
-def test_cog_ctor():
-    bot = mock.Mock()
-    cog = cogs.BaseCog(bot)
-
-    assert cog.bot is bot
+@pytest.fixture
+def bot(mocker):
+    return mocker.Mock(spec=commands.Bot)
 
 
-class TestAllSubclasses:
-    @pytest.fixture()
-    def cls_mock(self):
-        return mock.MagicMock()
-
-    def test_no_subclasses(self, cls_mock):
-        cls_mock.__subclasses__ = mock.Mock(return_value=[])
-
-        flatiter = cogs.all_subclasses(cls_mock)
-        with pytest.raises(StopIteration):
-            next(flatiter)
-
-    def test_no_nested_subclasses(self, cls_mock):
-        flatlist = []
-
-        for i in range(5):
-            cur = mock.MagicMock()
-            cur.__subclasses__ = mock.Mock(return_value=[])
-            flatlist.append(cur)
-
-        cls_mock.__subclasses__ = mock.Mock(return_value=flatlist)
-
-        flatiter = cogs.all_subclasses(cls_mock)
-
-        for expected, got in zip(flatlist, flatiter):
-            assert expected is got
-
-    def test_nested_subclasses(self, cls_mock):
-        cur, sub = mock.MagicMock(), mock.MagicMock()
-        flatlist = [cur]
-        cls_mock.__subclasses__ = mock.Mock(return_value=[cur])
-
-        for i in range(5):
-            cur.__subclasses__ = mock.Mock(return_value=[sub])
-            sub.__subclasses__ = mock.Mock(return_value=[])
-            flatlist.append(sub)
-            cur, sub = sub, mock.MagicMock()
-
-        flatiter = cogs.all_subclasses(cls_mock)
-
-        for expected, got in zip(flatlist, flatiter):
-            assert expected is got
-
-    def test_mixed(self, cls_mock):
-        cls_list = [mock.MagicMock() for i in range(5)]
-        flatlist = cls_list[:]
-
-        for cls in cls_list:
-            cur, sub = mock.MagicMock(), mock.MagicMock()
-            cls.__subclasses__ = mock.Mock(return_value=[cur])
-            flatlist.append(cur)
-
-            for i in range(5):
-                cur.__subclasses__ = mock.Mock(return_value=[sub])
-                sub.__subclasses__ = mock.Mock(return_value=[])
-                flatlist.append(sub)
-                cur, sub = sub, mock.MagicMock()
-
-        cls_mock.__subclasses__ = mock.Mock(return_value=cls_list)
-        flatiter = cogs.all_subclasses(cls_mock)
-
-        for expected, got in zip(flatlist, flatiter):
-            assert expected is got
+@pytest.fixture
+def cog_list(request, mocker):
+    mocker.patch('cardinal.cogs.cogs', new=request.param)
+    return request.param
 
 
-class TestSetup:
-    @pytest.fixture()
-    def patches(self, mocker):
-        iter_modules = mocker.patch('pkgutil.iter_modules')
-        import_module = mocker.patch('importlib.import_module')
-        all_subclasses = mocker.patch('cardinal.cogs.all_subclasses')
+@pytest.mark.parametrize(
+    ['cog_list'],
+    [
+        ([],),
+        ([mock.MagicMock(__name__='test mock 1')],),
+        ([mock.MagicMock(__name__='test mock 2', side_effect=Exception())],),
+        ([
+             mock.MagicMock(__name__='test mock 3'),
+             mock.MagicMock(__name__='test mock 4', side_effect=Exception()),
+             mock.MagicMock(__name__='test mock 5')
+         ],)
+    ],
+    indirect=['cog_list']
+)
+def test_setup(bot, cog_list):
+    cogs.setup(bot)
 
-        return {
-            'iter_modules': iter_modules,
-            'import_module': import_module,
-            'all_subclasses': all_subclasses
-        }
+    i = 0  # Index for call args list
+    for cog in cog_list:
+        cog.assert_called_once_with(bot)
 
-    @pytest.fixture(params=[
-        [],
-        [(mock.Mock(), 'test_module', False),
-         (mock.Mock(), 'other_test_module', True),
-         (mock.Mock(), 'another_test_module', False)]
-    ])
-    def modules(self, request, patches):
-        patches['iter_modules'].return_value = request.param
-        return request.param
-
-    @pytest.fixture(params=[
-        [],
-        [mock.MagicMock(__name__='test mock', return_value=mock.Mock())]
-    ])
-    def subclasses(self, request, patches):
-        patches['all_subclasses'].return_value = request.param
-        return request.param
-
-    def test_no_exception(self, patches, caplog, modules, subclasses):
-        bot = mock.Mock(spec=commands.Bot)
-
-        with caplog.at_level(logging.INFO, logger=cogs.__name__):
-            cogs.setup(bot)
-
-        patches['iter_modules'].assert_called_once_with(cogs.__path__)
-        patches['all_subclasses'].assert_called_once_with(cogs.BaseCog)
-        for record in caplog.records:
-            assert record.levelname != 'ERROR'
-
-        for _, mod_name, is_pkg in modules:
-            if not is_pkg:
-                patches['import_module'].assert_any_call('.' + mod_name, cogs.__name__)
-
-        if all(is_pkg for *_, is_pkg in modules):
-            patches['import_module'].assert_not_called()
-
-        if not subclasses:
-            bot.add_cog.assert_not_called()
-
-        for cls in subclasses:
-            assert (cogs.__name__, logging.INFO,
-                    'Initializing "{}".'.format(cls.__name__)) in caplog.record_tuples
-
-            cls.assert_called_with(bot)
-            bot.add_cog.assert_any_call(cls.return_value)
-
-    def test_import_exception(self, patches, caplog, modules):
-        bot = mock.Mock(spec=commands.Bot)
-        patches['import_module'].side_effect = (Exception(), None)
-
-        cogs.setup(bot)
-
-        if all(is_pkg for *_, is_pkg in modules):
-            patches['import_module'].assert_not_called()
-        else:
-            assert patches['import_module'].called
-            assert 'ERROR' in [record.levelname for record in caplog.records]
-
-        for _, mod_name, is_pkg in modules:
-            if not is_pkg:
-                patches['import_module'].assert_any_call('.' + mod_name, cogs.__name__)
-
-    @pytest.mark.usefixtures('patches')
-    def test_init_exception(self, caplog, subclasses):
-        bot = mock.Mock(spec=commands.Bot)
-
-        if subclasses:
-            subclasses[0].side_effect = Exception()
-
-        cogs.setup(bot)
-
-        if subclasses:
-            assert 'ERROR' in [record.levelname for record in caplog.records]
-
-        for cls in subclasses:
-            cls.assert_any_call(bot)
-
-            if not cls.side_effect:
-                bot.add_cog.assert_any_call(cls.return_value)
+        if cog.side_effect and i < len(bot.add_cog.call_args_list):
+            assert ((cog.return_value,), {}) != bot.add_cog.call_args_list[i]
+        elif not cog.side_effect:
+            assert i < len(bot.add_cog.call_args_list)
+            assert ((cog.return_value,), {}) == bot.add_cog.call_args_list[i]
+            i += 1  # Only increment if mock was called
