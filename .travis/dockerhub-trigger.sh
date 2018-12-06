b732c19b76b538ef9086c3d39b2bca17a51d1f30
Simon Engmann <simon.engmann@tu-dortmund.de>
2018-12-06 16:03:36 +0100

    Improve (?) Docker Hub build trigger from Travis

---
+++ .travis/dockerhub-trigger.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Don't submit Docker builds for PRs
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    exit
+fi
+
+if [[ -n "$TRAVIS_TAG" ]]; then
+    type="Tag"
+    source="$TRAVIS_TAG"
+else
+    type="Branch"
+    source="$TRAVIS_BRANCH"
+fi
+
+curl --data "source_type=${type}" --data "source_name=${source}" -X POST "https://registry.hub.docker.com/u/fallenwarrior2k/cardinal.py/trigger/${DOCKERHUB_TOKEN}/"
