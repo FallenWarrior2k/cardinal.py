3d457c4f36c027d6265d7e41011089cd233bf6e9
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-05-23 20:16:41 +0200

    Stopping a channel now restricts reactions

--- src/cardinal/cogs/stop.py
+++ src/cardinal/cogs/stop.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from logging import getLogger
 
 from discord.ext.commands import bot_has_permissions, group, guild_only, has_permissions
@@ -6,6 +7,8 @@ from ..utils import maybe_send
 from .basecog import BaseCog
 
 logger = getLogger(__name__)
+
+_perms = namedtuple('perms', ['send_messages', 'add_reactions'])
 
 
 class Stop(BaseCog):
@@ -39,7 +42,7 @@ class Stop(BaseCog):
     @stop.command()
     async def on(self, ctx):
         """
-        Restrict messaging to a channel for everyone.
+        Restrict messaging and reactions to a channel for everyone.
         Note that the user issueing this command should probably have some form of way to
         still write to the channel, or they will need to release the lock manually.
         """
@@ -48,22 +51,28 @@ class Stop(BaseCog):
         await maybe_send(ctx, 'Sending messages to this channel has been restricted.')
 
         overwrite = channel.overwrites_for(ctx.guild.default_role)
-        self.perm_cache.setdefault(channel.id, overwrite.send_messages)
+        self.perm_cache.setdefault(channel.id,
+                                   _perms(overwrite.send_messages, overwrite.add_reactions))
         overwrite.send_messages = False
+        overwrite.add_reactions = False
         await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
 
     @stop.command()
     async def off(self, ctx):
         """
         Re-open a channel after it was locked down.
-        This restores the default role's permission to send messages to this channel
-        to its previous value.
-        If the channel was not previously locked down, that permission is set to the global default.
+        This restores the default role's modified permissions,
+        i.e. Send Messages and Add Reactions, to their previous values.
+        If the channel was not previously locked down, nothing happens.
         """
         channel = ctx.channel
 
         overwrite = channel.overwrites_for(ctx.guild.default_role)
-        overwrite.send_messages = self.perm_cache.pop(channel.id, None)
+
+        perms = self.perm_cache.pop(channel.id, _perms(None, None))
+        overwrite.send_messages = perms.send_messages
+        overwrite.add_reactions = perms.add_reactions
+
         overwrite = overwrite if not overwrite.is_empty() else None  # Clear overwrite if empty
         await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
 
